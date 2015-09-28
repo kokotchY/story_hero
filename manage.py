b11f3d153af2652f63da174d9e2981b00d11f751
Sebastien Canart <kokotchy@gmail.com>
2015-09-28 21:28:16 +0200

    Display next steps, edit a step

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 from app import create_app, db
+from app.models import User, Story
 
 app = create_app()
 manager = Manager(app)
@@ -13,6 +14,14 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
 
+@manager.command
+def init():
+    user = User('kokotchy', 'address@host.com')
+    db.session.add(user)
+    db.session.commit()
+    story = Story('This is a story', user.id)
+    db.session.add(story)
+    db.session.commit()
 
 if __name__ == "__main__":
     manager.run()
