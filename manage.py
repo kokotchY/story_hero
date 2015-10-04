e0fbc600f4019e4ce8a3736741bc9f662091ffc9
Sebastien Canart <kokotchy@gmail.com>
2015-10-04 10:50:49 +0200

    Add list_routes command

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask import url_for
 from app import create_app, db
 from app.models import User, Story
 
@@ -22,6 +23,21 @@ def init():
     story = Story('This is a story', user.id)
     db.session.add(story)
     db.session.commit()
+
+@manager.command
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+    for line in sorted(output):
+        print(line)
 
 if __name__ == "__main__":
     manager.run()
